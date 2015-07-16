ade178d2963df3700b2eac9225d262b76dd6d44a
Justin Lee <justinj.lee23@gmail.com>
2015-07-15 22:04:58 -0700

    calculator ios using swift

---
+++ Swift Tutorial/Swift Tutorial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Tutorial
+//
+//  Created by Justin Lee on 1/10/15.
+//  Copyright (c) 2015 Justin Lee. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
